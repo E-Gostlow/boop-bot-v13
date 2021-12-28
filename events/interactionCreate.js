66c8043dcf96025a03ee2b25fe2f26c17dc57a22
Emile Gostlow <emilegostlow@icloud.com>
2021-12-28 22:43:35 +0000

    Upload files

---
+++ events/interactionCreate.js
@@ -0,0 +1,6 @@
+module.exports = async (client, interaction) => {
+    const { commandName } = interaction;
+	if (!interaction.isCommand() || !client.commands.has(commandName)) return;
+
+	await client.commands.get(commandName).execute(interaction);
+};
---
+++ index.js
@@ -0,0 +1,68 @@
+//const { TOKEN, CLIENT_ID, GUILD_ID } = require("./config.json");
+const { options } = require("./assets/colours.json");
+const { Client, Collection } = require("discord.js");
+const { Routes } = require("discord-api-types/v9");
+const { readdirSync, readdir } = require("fs");
+const { REST } = require("@discordjs/rest");
+
+const client = new Client({
+	intents: [
+		"GUILDS"
+	]
+});
+
+client.commands = new Collection();
+client.colours = options;
+client.token = process.env.TOKEN;
+let cmds = [];
+
+const commandFiles = readdirSync("./commands/").filter(f => f.endsWith(".js"));
+
+for (const file of commandFiles){
+	const command = require(`./commands/${file}`);
+
+	client.commands.set(command.data.name, command);
+}
+
+const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log("Setting slash commands..");
+
+		for (let i = 0; i < client.commands.toJSON().length; i++) {
+			cmds.push(client.commands.toJSON()[i].data.toJSON());
+		}
+
+		await rest.put(
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+			{
+				body: cmds
+			}
+		);
+
+		console.log("Success!");
+    } catch (err) {
+		console.error();
+    }
+})();
+
+readdir("./events/", (err, files) => {
+	if (err) return console.error();
+	
+	files.forEach(file => {
+		if (!file.endsWith(".js")) return;
+
+		const event = require(`./events/${file}`);
+		let eventName = file.split(".")[0];
+
+		if (file.toLowerCase() == "ready.js"){
+			client.once(eventName, event.bind(null, client));
+			return;
+		}
+
+		client.on(eventName, event.bind(null, client));
+	});
+});
+
+client.login(process.env.TOKEN);
